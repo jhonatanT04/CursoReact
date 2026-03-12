69040f64a350a5c6204ed18ec22bde6d9cba88be
jhonatanT04 <venotacu@gmail.com>
2026-03-11 21:39:53 -0500

    Se usa useEffect para cambiar el estado del puntero

--- 02-mouse-react/src/App.jsx
+++ 02-mouse-react/src/App.jsx
@@ -1,33 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
+const FollowMause = () => {
+  const [enable, setEnable] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  useEffect(() => {
+    console.log('Efecto', { enable })
+
+    const hadleMove = (event) => {
+      const { clientX, clientY } = event
+
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enable) {
+      window.addEventListener('pointermove', hadleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', hadleMove)
+      console.log("Clenap")
+    }
+
+  }, [enable])
+  
+  return (
+    <main>
+    { enable && <div style={{
+      position: 'absolute',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      border: '1px solid #fff',
+      borderRadius: '50%',
+      opacity: 0.8,
+      pointerEvents: 'none',
+      left: -25,
+      top: -25,
+      width: 50,
+      height: 50,
+      transform: `translate(${position.x}px, ${position.y}px)`
+    }}
+    /> }
+      <button onClick={() => { setEnable(!enable) }}>
+        {enable ? 'Desactivar' : 'Activar'} seguimiento
+      </button>
+    </main>
+  )
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <FollowMause/>
     </>
   )
 }
